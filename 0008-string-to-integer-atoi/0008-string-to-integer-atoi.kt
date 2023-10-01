72a12bc7fcb535d0e1b3e9c4b114bbc62f27c120
Natan Alexandre <62835245+natan-alex@users.noreply.github.com>
2023-10-01 13:39:30 -0300

    Time: 214 ms (12.87%), Space: 37.2 MB (27.04%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.kt
@@ -0,0 +1,48 @@
+class Solution {
+    fun myAtoi(s: String): Int {
+        var startIndex = -1
+        var isNegative = false
+        var signDefined = false
+
+        var i = 0
+        
+        while (i < s.length) {
+            when (s[i]) {
+                '+' -> {
+                    if (startIndex >= 0 || signDefined) { break }
+                    signDefined = true
+                }
+                '-' -> {
+                    if (startIndex >= 0 || signDefined) { break }
+                    isNegative = true
+                    signDefined = true
+                }
+                ' ' -> {
+                    if (startIndex >= 0 || signDefined) { break }
+                }
+                in '0'..'9' -> {
+                    if (startIndex < 0) {
+                        startIndex = i
+                    }
+                }
+                else -> {
+                    break
+                }
+            }
+            
+            i++
+        }
+        
+        if (startIndex < 0) return 0
+        
+        val numberAsString = s.substring(startIndex, i)
+        
+        var number = numberAsString.toIntOrNull()
+        
+        if (number == null) {
+            return if (isNegative) Int.MIN_VALUE else Int.MAX_VALUE
+        }
+        
+        return if (isNegative) number * -1 else number
+    }
+}
